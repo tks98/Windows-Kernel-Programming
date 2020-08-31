1992d940a4a4071ee49da7d57b2e2895af80da49
tsuika98 <tsuika98@gmail.com>
2020-08-30 21:00:17 -0500

    comment cpp source

--- Chapter2/Chapter2.cpp
+++ Chapter2/Chapter2.cpp
@@ -1,14 +1,17 @@
-#include <ntddk.h> 
+#include <ntddk.h> // imported to use driver types
 
+// called before the driver is unloaded
+// should be used to free resources used on DriverEntry so there is not a leak in kernel memory
 void SampleUnload(_In_ PDRIVER_OBJECT DriverObject) 
 { 
 	UNREFERENCED_PARAMETER(DriverObject); 
 }
 
-extern "C" 
-NTSTATUS 
+extern "C" NTSTATUS // Gives DriveryEntry C linkage
+
+// EntryPoint to the driver application
 DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
-	UNREFERENCED_PARAMETER(RegistryPath);
-	DriverObject->DriverUnload = SampleUnload;
-	return STATUS_SUCCESS;
+	UNREFERENCED_PARAMETER(RegistryPath); // used so driver compiles, we do not use RegistryPath
+	DriverObject->DriverUnload = SampleUnload; // set to call SampleUnload before driver exits
+	return STATUS_SUCCESS; 
 }
