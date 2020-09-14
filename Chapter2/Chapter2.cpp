499b7813378c907d09ea2dc2d0c0d7270d62a5c7
tsuika98 <tsuika98@gmail.com>
2020-09-13 21:52:41 -0500

    add debugging prints, finish chapter 2

--- Chapter2/Chapter2.cpp
+++ Chapter2/Chapter2.cpp
@@ -4,7 +4,8 @@
 // should be used to free resources used on DriverEntry so there is not a leak in kernel memory
 void SampleUnload(_In_ PDRIVER_OBJECT DriverObject) 
 { 
-	UNREFERENCED_PARAMETER(DriverObject); 
+	UNREFERENCED_PARAMETER(DriverObject);
+	KdPrint(("Sample driver unload called\n"));
 }
 
 extern "C" NTSTATUS // Gives DriveryEntry C linkage
@@ -13,5 +14,6 @@ extern "C" NTSTATUS // Gives DriveryEntry C linkage
 DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
 	UNREFERENCED_PARAMETER(RegistryPath); // used so driver compiles, we do not use RegistryPath
 	DriverObject->DriverUnload = SampleUnload; // set to call SampleUnload before driver exits
+	KdPrint(("Sample driver initialized successfully"));
 	return STATUS_SUCCESS; 
 }
