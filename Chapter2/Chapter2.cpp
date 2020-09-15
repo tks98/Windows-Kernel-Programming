6ffc1f364c285b1ec007759980e0d2438b3cb167
tsuika98 <tsuika98@gmail.com>
2020-09-14 20:59:11 -0500

    finish chapter 2 for real

--- Chapter2/Chapter2.cpp
+++ Chapter2/Chapter2.cpp
@@ -2,9 +2,17 @@
 
 // called before the driver is unloaded
 // should be used to free resources used on DriverEntry so there is not a leak in kernel memory
-void SampleUnload(_In_ PDRIVER_OBJECT DriverObject) 
+void DriverUnload(_In_ PDRIVER_OBJECT DriverObject) 
 { 
-	UNREFERENCED_PARAMETER(DriverObject); 
+	UNREFERENCED_PARAMETER(DriverObject);
+	KdPrint(("Unloading driver"));
+}
+
+void PrintOSInfo() {
+	_Out_ RTL_OSVERSIONINFOW WindowsInfo; // declare structure that holds OS info
+	RtlZeroMemory(&WindowsInfo, sizeof(PRTL_OSVERSIONINFOW)); // allocate memory for the structure
+	RtlGetVersion(&WindowsInfo); // populate the structure
+	KdPrint(("Windows version %d.%d, build %ul\n", WindowsInfo.dwMajorVersion, WindowsInfo.dwMinorVersion, WindowsInfo.dwBuildNumber)); // print os info
 }
 
 extern "C" NTSTATUS // Gives DriveryEntry C linkage
@@ -12,14 +20,9 @@ extern "C" NTSTATUS // Gives DriveryEntry C linkage
 // EntryPoint to the driver application
 DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
 	UNREFERENCED_PARAMETER(RegistryPath); // used so driver compiles, we do not use RegistryPath
-	DriverObject->DriverUnload = SampleUnload; // set to call SampleUnload before driver exits
+	DriverObject->DriverUnload = DriverUnload; // set to call SampleUnload before driver exits
+	PrintOSInfo();
 	return STATUS_SUCCESS; 
 }
 
-void PrintVersion() {
-	_Out_ RTL_OSVERSIONINFOW WindowsInfo; // declare structure that holds OS info
-	RtlZeroMemory(&WindowsInfo, sizeof(PRTL_OSVERSIONINFOW)); // allocate memory for the structure
-	RtlGetVersion(&WindowsInfo); // populate the structure
-	KdPrint(("Windows version %d.%d, build %ul\n", WindowsInfo.dwMajorVersion, WindowsInfo.dwMinorVersion, WindowsInfo.dwBuildNumber)); // print os info
-	return;
-}
+
