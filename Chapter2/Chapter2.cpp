da974fe8dcd139feace845c36bc1427cb8be0e8f
tsuika98 <tsuika98@gmail.com>
2020-09-14 20:56:01 -0500

    print os info

--- Chapter2/Chapter2.cpp
+++ Chapter2/Chapter2.cpp
@@ -4,8 +4,7 @@
 // should be used to free resources used on DriverEntry so there is not a leak in kernel memory
 void SampleUnload(_In_ PDRIVER_OBJECT DriverObject) 
 { 
-	UNREFERENCED_PARAMETER(DriverObject);
-	KdPrint(("Sample driver unload called\n"));
+	UNREFERENCED_PARAMETER(DriverObject); 
 }
 
 extern "C" NTSTATUS // Gives DriveryEntry C linkage
@@ -14,6 +13,13 @@ extern "C" NTSTATUS // Gives DriveryEntry C linkage
 DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
 	UNREFERENCED_PARAMETER(RegistryPath); // used so driver compiles, we do not use RegistryPath
 	DriverObject->DriverUnload = SampleUnload; // set to call SampleUnload before driver exits
-	KdPrint(("Sample driver initialized successfully"));
 	return STATUS_SUCCESS; 
+}
+
+void PrintVersion() {
+	_Out_ RTL_OSVERSIONINFOW WindowsInfo; // declare structure that holds OS info
+	RtlZeroMemory(&WindowsInfo, sizeof(PRTL_OSVERSIONINFOW)); // allocate memory for the structure
+	RtlGetVersion(&WindowsInfo); // populate the structure
+	KdPrint(("Windows version %d.%d, build %ul\n", WindowsInfo.dwMajorVersion, WindowsInfo.dwMinorVersion, WindowsInfo.dwBuildNumber)); // print os info
+	return;
 }
